428337db36012488032ec5d7d29851f402c7a0a4
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2023-03-13 17:01:04 -0300

    Created All Commands

---
+++ GamesBlog.Domain/Commands/ArticleCommand.cs
@@ -0,0 +1,21 @@
+﻿using GamesBlog.Domain.Entitties;
+using GamesBlog.Domain.Commands.Contracts;
+
+namespace GamesBlog.Domain.Commands
+{
+    public class ArticleCommand : ICommand
+    {
+        public Title ArticleTitle { get; private set; }
+        public Paragraph ArticleParagraph { get; private set; }
+        public string? Image { get; private set; }
+        public bool IsValid { get; private set; }
+
+        public ArticleCommand(Title articleTitle, Paragraph articleParagraph, string? image)
+        {
+            ArticleTitle = articleTitle;
+            ArticleParagraph = articleParagraph;
+            Image = image;
+        }
+
+    }
+}
---
+++ GamesBlog.Domain/Commands/TitleCommand.cs
@@ -0,0 +1,15 @@
+﻿using GamesBlog.Domain.Commands.Contracts;
+
+namespace GamesBlog.Domain.Commands
+{
+    public class TitleCommand : ICommand
+    {
+        public string TitleName { get; private set; }
+        public bool IsValid { get; private set; }
+
+        public TitleCommand(string titleName)
+        {
+            TitleName = titleName;
+        }
+    }
+}
